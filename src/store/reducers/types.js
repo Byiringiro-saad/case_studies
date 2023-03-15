74735fae01295ea0ab7702a38aff64e5fef906e5
Byiringiro Saad <byiringirosaad@gmail.com>
2023-03-15 10:09:40 +0200

    moved everything to cloud

--- src/store/reducers/types.js
+++ src/store/reducers/types.js
@@ -2,18 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const typesSlice = createSlice({
   name: "cases types",
-  initialState: {
-    types: [
-      "Financial Services",
-      "Banking",
-      "Legal",
-      "Sales",
-      "Healthcare",
-      "Media and Entertainment",
-      "E-commerce",
-      "Surveillance",
-    ],
-  },
+  initialState: {},
   reducers: {},
 });
 
